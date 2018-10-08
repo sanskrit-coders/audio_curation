4795a137344377031b73a2d327b52257185f38bf
vvasuki <vishvas.vasuki@gmail.com>
2018-10-07 21:50:22 -0700

    Content update

--- audio_curation/archive_utility.py
+++ audio_curation/archive_utility.py
@@ -127,4 +127,4 @@ class ArchiveAudioItem(object):
                         metadata=dict(title=mp3_metadata.title, album=mp3_metadata.album,
                                       album_artist=mp3_metadata.album_artist,
                                       artist=mp3_metadata.artist, creator=mp3_metadata.artist),
-                        target=remote_name))
+                        target=os.path.join("files", remote_name)))
