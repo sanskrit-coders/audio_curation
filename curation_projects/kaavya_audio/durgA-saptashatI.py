ef163bf8190c602faffef847723f23fb2076bd70
vvasuki <vishvas.vasuki@gmail.com>
2019-10-05 16:19:34 +0530

    Content update

--- curation_projects/kaavya_audio/durgA-saptashatI.py
+++ curation_projects/kaavya_audio/durgA-saptashatI.py
@@ -41,10 +41,38 @@ class NormalizedFilesRepo(audio_repo.NormalizedRepo):
     archive_id="durgA-saptashatI"
 
 
+class SpeedFileRepo(audio_repo.SpeedFileRepo):
+    metadata = {
+        "title" : "durgA-saptashatI दुर्गासप्तशती चण्डी-पाठः",
+        "description" : """
+          दुर्गासप्तशती मार्कण्डेयपुराणात्। durgAsaptashatI from mArkaNDeyapurANa.  
+          Chanted by brAhmaNas from MH, likely. Obtained via puja.net.
+          Aka ChaNDI-pAThaH. 1.5x speed.
+          भवद्योगदानं‌ काङ्क्ष्यते - https://sanskrit.github.io/projects/audio/kaavya-audio/index.html
+    """
+    }
+
+    archive_id = NormalizedFilesRepo.archive_id + "-150p-speed"
+
+    def update_metadata(self, mp3_files):
+        """ Update mp3 metadata of a bunch of files. Meant to be overridden.
+
+        :param mp3_files: List of :py:class:mp3_utility.Mp3File objects
+        """
+        for mp3_file in mp3_files:
+            mp3_file.metadata = mp3_utility.Mp3Metadata(
+                title=mp3_file.title_from_filename(),
+                album="durgA-saptashatI दुर्गासप्तशती 1.5x",
+                artist="MH brAhmaNas ब्राह्मणाः"
+            )
+            mp3_file.save_metadata()
+
+
 def update_repo(gmusic_client, dry_run=False):
     repo = RepoBase(repo_paths=[os.path.join("/home/vvasuki/kAvya-audio/durgA-saptashatI")])
     archive_audio_item = archive_utility.ArchiveAudioItem(archive_id=NormalizedFilesRepo.archive_id)
     archive_audio_item.update_metadata(metadata=NormalizedFilesRepo.metadata)
+    logging.info(pprint.pformat(repo.reprocess(dry_run=dry_run)))
     normalized_files_repo = audio_repo.NormalizedRepo(base_repo=repo, archive_audio_item=archive_audio_item, gmusic_client=gmusic_client)
     logging.info(pprint.pformat(normalized_files_repo.reprocess(dry_run=dry_run)))
 
