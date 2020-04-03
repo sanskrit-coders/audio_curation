d7f224acd377baf50a176d2ebc7cd77171c27b3b
vvasuki <vishvas.vasuki@gmail.com>
2020-04-03 11:38:50 +0530

    CU

--- curation_projects/kaavya_audio/raamacharitamaanasa.py
+++ curation_projects/kaavya_audio/raamacharitamaanasa.py
@@ -22,6 +22,8 @@ class Ramcharitmanas(audio_repo.BaseAudioRepo):
         रामचरितमानस। 
         Ramcharitmanas version of Ramayan complete by All India Radio.
         Derived from https://archive.org/details/EPI63 to fix file ordering.
+        
+        Contributions welcome: https://sanskrit.github.io/projects/audio/kAvya-audio/
         """
     }
     archive_id="rAmcharitmAnas-AIR"
@@ -31,7 +33,7 @@ class Ramcharitmanas(audio_repo.BaseAudioRepo):
 def update(gmusic_client, dry_run):
     archive_audio_item = archive_utility.ArchiveAudioItem(archive_id=Ramcharitmanas.archive_id, metadata=Ramcharitmanas.metadata)
     repo = Ramcharitmanas(repo_paths=repo_paths, archive_audio_item=archive_audio_item, gmusic_client=gmusic_client)
-    logging.info(pprint.pformat(repo.reprocess(dry_run=dry_run)))
+    # logging.info(pprint.pformat(repo.reprocess(dry_run=dry_run)))
     archive_audio_item.update_metadata(metadata=Ramcharitmanas.metadata)
     
     
@@ -39,6 +41,6 @@ def update(gmusic_client, dry_run):
 
 if __name__ == "__main__":
     gmusic_client = None
-    gmusic_client = google_music.GMusicClient(oauth_file_path="/home/vvasuki/sysconf/kunchikA/google/sanskritnlp/oauth_access_token_gmusic.json", username="vishvas.vasuki@gmail.com")
+    # gmusic_client = google_music.GMusicClient(oauth_file_path="/home/vvasuki/sysconf/kunchikA/google/sanskritnlp/oauth_access_token_gmusic.json", username="vishvas.vasuki@gmail.com")
     update(gmusic_client=gmusic_client, dry_run=False)
     pass
