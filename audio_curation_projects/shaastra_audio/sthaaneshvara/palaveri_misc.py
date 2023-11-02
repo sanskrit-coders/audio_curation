e479ed5337e736e3b9ca456f5b5849f567a9bac9
vvasuki <vishvas.vasuki@gmail.com>
2023-11-02 09:21:07 +0530

    CU

--- audio_curation_projects/shaastra_audio/sthaaneshvara/palaveri_misc.py
+++ audio_curation_projects/shaastra_audio/sthaaneshvara/palaveri_misc.py
@@ -8,7 +8,7 @@ from curation_utils import archive_utility
 from audio_utils import mp3_utility
 
 
-BASE_DIR = "/run/media/vvasuki/vData/audio/curation/git/shAstra-audio/palaveri-laxmInArAyaNaH/"
+BASE_DIR = "/run/media/vvasuki/vData/audio/curation/git/shAstra-audio/palaveri-laxmInarasiMhaH/"
 GENERIC_DESCRIPTION = """
 पलवेरि-लक्ष्मीनारायणार्य बोधनानि।  
 **अधिकारिभिर् एव तद्-अनुज्ञया श्राव्यम्, नेतरैर्** इति प्रवक्तुर् निर्बन्धः।  
@@ -18,6 +18,8 @@ GENERIC_DESCRIPTION = """
 
 
 if __name__ == '__main__':
-  archive_utility.update_item(item_id="laxmI-narasiMhaH_RTS", dir_path=os.path.join(BASE_DIR, "rahasya-traya-sAraH"), metadata={"description":GENERIC_DESCRIPTION})
+  # archive_utility.update_item(item_id="laxmI-narasiMhaH_RTS", dir_path=os.path.join(BASE_DIR, "rahasya-traya-sAraH"), metadata={"description":GENERIC_DESCRIPTION})
+  # archive_utility.update_item(item_id="laxmI-narasiMhaH_BS", dir_path=os.path.join(BASE_DIR, "brahma-sUtram"), metadata={"description":GENERIC_DESCRIPTION})
   archive_utility.update_item(item_id="laxmI-narasiMhaH_BS", dir_path=os.path.join(BASE_DIR, "brahma-sUtram"), metadata={"description":GENERIC_DESCRIPTION})
+
   pass
